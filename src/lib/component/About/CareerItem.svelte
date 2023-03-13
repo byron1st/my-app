11c2f1690f0e09d018105ba0950559a4df2068a5
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2023-03-13 16:51:45 +0900

    경력 추가

---
+++ src/lib/component/About/CareerItem.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import dayjs from 'dayjs';
+	import Briefcase from '$lib/icons/Briefcase.svelte';
+	import CalendarDays from '$lib/icons/CalendarDays.svelte';
+	import classnames from 'classnames';
+
+	export let work: string;
+	export let position: string;
+	export let link: string;
+	export let from: Date;
+	export let to: Date | undefined = undefined;
+	export let isLast = false;
+
+	$: duration = `${dayjs(from).format('YYYY-MM-DD')} - ${
+		to ? dayjs(to).format('YYYY-MM-DD') : 'Present'
+	}`;
+</script>
+
+<div class="flex flex-row items-center">
+	<div
+		class={classnames('h-full flex-1', {
+			'border-b border-b-slate-800/30 dark:border-b-slate-50/30': !isLast
+		})}
+	>
+		<div class="flex flex-col py-2">
+			<a class="underline" href={link} target="_blank" rel="noreferrer">{work}</a>
+			<div class="flex flex-row items-center gap-1">
+				<Briefcase class="h-3 w-3" />
+				<p class="text-xs">{position}</p>
+			</div>
+			<div class="flex flex-row items-center gap-1">
+				<CalendarDays class="h-3 w-3" />
+				<p class="text-xs">{duration}</p>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,21 @@
-<div />
+<script lang="ts">
+	import CareerItem from '$lib/component/About/CareerItem.svelte';
+	import Section from '$lib/component/About/Section.svelte';
+	// 경력
+	// 기술 (클릭해서 모달로 자세한 설명 띄우기. 관련 프로젝트/강의 reference 포함)
+	// 회사 프로젝트
+	// 개인 프로젝트
+	// 강의
+</script>
+
+<div class="flex w-full flex-col gap-6">
+	<Section title="경력">
+		<CareerItem
+			work="빅픽처랩(주)"
+			position="CTO, 풀스텍 개발자"
+			link="https://bigpicturelabs.io"
+			from={new Date('2018-05-01')}
+			isLast
+		/>
+	</Section>
+</div>
