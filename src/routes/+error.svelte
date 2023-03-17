2ce62342c56cd2de4a76cb274f5583087930b033
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2023-03-17 13:08:05 +0900

    Error 페이지 수정

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: statusCode = $page.error?.message === 'Not Found' ? 404 : 500;
+	$: statusMessage = $page.error?.message === 'Not Found' ? 'Not Found' : 'Internal Server Error';
+	$: description =
+		$page.error?.message === 'Not Found'
+			? '해당 URL에 맞는 페이지가 없습니다'
+			: '서버에 문제가 발생했습니다';
+</script>
+
 <div class="h-screen-but-header flex w-full flex-col items-center justify-center">
 	<div class="flex flex-row items-center gap-8">
-		<p class="text-5xl font-bold">404</p>
+		<p class="text-5xl font-bold">{statusCode}</p>
 
 		<div class="h-full border-l border-l-slate-800 dark:border-l-slate-100" />
 
 		<div class="flex flex-col">
-			<p class="text-3xl">Not Found</p>
-			<p>해당 URL에 맞는 페이지가 없습니다</p>
+			<p class="text-3xl">{statusMessage}</p>
+			<p>{description}</p>
 		</div>
 	</div>
 </div>
