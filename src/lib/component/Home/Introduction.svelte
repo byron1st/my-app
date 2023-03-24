fa34f5ff09ba0d337eb395b69a7e799b10e3d804
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2023-03-24 16:43:22 +0900

    TODO 코멘트 추가

--- src/lib/component/Home/Introduction.svelte
+++ src/lib/component/Home/Introduction.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
 	import Box from '$lib/component/Box.svelte';
+
+	// TODO: DB로 이동
 </script>
 
 <Box class="flex flex-col px-4 py-2" enableHover={false}>
-	<p>저의 홈페이지에 오신 것을 환영합니다!</p>
-	<p>전 웹서비스 설계와 개발을 좋아하는 프로그래머입니다.</p>
-	<br />
-	<p>
-		제가 누군지 궁금하시면 <a href="/about"><b>about:me</b></a>를,
-	</p>
-	<p>
-		이 웹사이트가 궁금하시면
-		<a href="/site"><b>about:site</b></a>를 클릭해주세요.
-	</p>
+	저의 홈페이지에 오신 것을 환영합니다!<br />
+	전 소프트웨어 설계와 개발을 좋아하는 프로그래머입니다.<br />
+	지금까지는 주로 B2B 웹 서비스와 프라이빗 블록체인 기반 서비스 및 플랫폼을 설계 및 개발했습니다.<br
+	/>
+	하지만 소프트웨어 개발이라면 분야, 백앤드/프론트앤드를 가리지 않고 좋아합니다.<br />
 </Box>
