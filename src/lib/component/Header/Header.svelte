aef1a68ffe75fbede81ffa6f6e684b72c7ef82b6
Hwi Ahn <hwi.ahn@bigpicturelabs.io>
2023-04-06 15:10:36 +0900

    로고, 아이콘, Favicon 교체

--- src/lib/component/Header/Header.svelte
+++ src/lib/component/Header/Header.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import MenuList from '$lib/component/Header/MenuList.svelte';
 	import DropdownMenuList from '$lib/component/Header/DropdownMenuList.svelte';
+	import Logo from '$lib/component/core/Logo.svelte';
 </script>
 
 <div class="flex h-12 w-full items-center justify-between gap-4 px-4 lg:w-[64rem]">
 	<a href="/" class="flex flex-row items-center gap-1">
-		<img src="/icon.min.svg" alt="profile" class="h-6 w-6 rounded-lg" />
-		<p class="font-bold">Hwi's Website</p>
+		<Logo />
 	</a>
 
 	<div class="hidden gap-4 sm:flex">
---
+++ src/lib/component/core/Logo.svelte
@@ -0,0 +1,1 @@
+<p id="logo" class="font-black italic text-blue-900 drop-shadow-md dark:text-blue-100">Hwi Ahn</p>
