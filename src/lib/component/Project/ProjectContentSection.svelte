717b09152bb4643b1af31b93e86ee937978c9712
Hwi Ahn <byron1st@icloud.com>
2023-10-05 21:57:10 +0900

    Remove unused components

--- src/lib/component/Project/ProjectContentSection.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	export let label: string;
-</script>
-
-<section class="mb-6">
-	<h1 class="text-2xl font-bold mb-2">{label}</h1>
-	<slot />
-</section>
--- src/lib/component/Project/ProjectContentSectionItem.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	export let label: string;
-	export let content: string;
-</script>
-
-<div class="flex flex-row mb-2">
-	<p class="shrink-0 w-24 font-bold">{label}</p>
-	<div>{@html content}</div>
-</div>
